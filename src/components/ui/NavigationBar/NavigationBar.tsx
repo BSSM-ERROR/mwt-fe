f801a0f803bbcc491698ff6546f2ca47da691b2a
winshine0326 <shane10340@gmail.com>
2025-12-09 12:19:37 +0900

    feat :: 채팅 버튼 클릭 시 /chat으로 이동

--- src/components/ui/NavigationBar/NavigationBar.tsx
+++ src/components/ui/NavigationBar/NavigationBar.tsx
@@ -1,10 +1,19 @@
 "use client";
+import { useRouter } from "next/navigation";
 import * as S from "./style";
 
 const NavBar = () => {
+  const router = useRouter();
+
+  const handleChatClick = () => {
+    router.push("/chat");
+  };
+
   return (
     <>
-      <S.Container></S.Container>
+      <S.Container>
+        <S.ChatButton onClick={handleChatClick} />
+      </S.Container>
     </>
   );
 };
--- src/components/ui/NavigationBar/style.ts
+++ src/components/ui/NavigationBar/style.ts
@@ -6,9 +6,9 @@ export const Container = styled.div`
   left: 50%;
   transform: translateX(-50%);
   width: 100%;
-  min-height: 10rem;
-  padding: 0.1em;
   max-width: 600px;
+  aspect-ratio: 393 / 104;
+  padding: 0.1em;
 
   &::before {
     content: "";
@@ -18,8 +18,8 @@ export const Container = styled.div`
     width: 100%;
     height: 100%;
     background-image: url("/assets/shell.svg");
-    background-size: 100% auto;
-    background-position: center bottom;
+    background-size: 100% 100%;
+    background-position: center;
     background-repeat: no-repeat;
     opacity: 0.8;
     z-index: -1;
@@ -32,4 +32,26 @@ export const MenuItem = styled.div`
   align-items: center;
   gap: 0.375rem;
   flex: 1 0 0;
+`;
+
+export const ChatButton = styled.button`
+  position: absolute;
+  top: 28.85%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 14.25%;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  z-index: 10;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    transform: translate(-50%, -50%) scale(0.95);
+  }
 `;
